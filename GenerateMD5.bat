25e0a692a8ab676be5b967467741c05a11e6d436
T-MaxWiese-T <johndeerewieoderwas@gmail.com>
2020-12-10 17:57:02 +0100

    v7.5.2

--- GenerateMD5.bat
+++ GenerateMD5.bat
@@ -3,7 +3,7 @@
 echo Producing Packages.md5
 
 cd .\System
-..\..\ContentExpansion\System\ucc.exe mastermd5 -c *.u -c SEF Community Mod v7/Content/*.ukx -c SEF Community Mod v7/Content/Maps/*.s4m -c SEF Community Mod v7/Content/*.utx
+..\..\ContentExpansion\System\ucc.exe mastermd5 -c *.u -c SEFCommunityModv7/Content/*.ukx -c SEFCommunityModv7/Content/Maps/*.s4m -c SEFCommunityModv7/Content/*.utx
 
 echo Produced entries:
 
